9f206ca7922ef2157bcbaa5880934499a960f614
harsha-mangena <harsha.mangena@gmail.com>
2024-06-08 15:28:17 -0400

    implementing exit

--- app/main.py
+++ app/main.py
@@ -3,16 +3,24 @@ import sys
 
 def main():
 
-    __pre_built_cmds = dict()
+    # __pre_built_cmds = {
+    #     "exit 0" : True,
+
+    # }
 
     # Wait for user input
     while True:
         sys.stdout.write("$ ")
         sys.stdout.flush()
         cmd = input()
-        if cmd not in __pre_built_cmds:
-            print("{}: command not found".format(cmd))
-            continue
+
+        # Break
+        if cmd == "exit 0":
+            break
+    
+        print("{}: command not found".format(cmd))
+        continue
+        
 
 
 

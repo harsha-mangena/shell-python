44de7b183b5ea3401ad6bb2951de76e713be69dc
harsha-mangena <harsha.mangena@gmail.com>
2024-06-08 15:20:37 -0400

    implementing unknown case

--- app/main.py
+++ app/main.py
@@ -3,11 +3,17 @@ import sys
 
 def main():
 
+    __pre_built_cmds = dict()
+
     sys.stdout.write("$ ")
     sys.stdout.flush()
 
     # Wait for user input
-    input()
+    cmd = input()
+
+    if cmd not in __pre_built_cmds:
+        print("{}: command not found".format(cmd))
+
 
 
 if __name__ == "__main__":
